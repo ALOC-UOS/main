cbfd197ff731bc4d07888ce62e7eee9710b1e6b0
Dodolist <dodolist3@gmail.com>
2024-03-07 13:20:58 +0900

    refactor: ListInput 컴포넌트화

---
+++ src/components/Input/ListInput/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { InputBox, InputList, InputItem, SelectedText, ArrowIcon } from './style';
+import InputArrowIcon from '../../../assets/input-arrow-icon.svg';
+
+const ListInput = ({
+  apiURL,
+  listName,
+  listData,
+  isOpenList,
+  selectedItemId,
+  selectedItemName,
+  openList,
+  selectItem
+}) => {
+  return (
+    <InputBox onClick={() => openList(listName)}>
+      <SelectedText>{selectedItemName}</SelectedText>
+      <ArrowIcon src={InputArrowIcon} selected={isOpenList === listName} />
+      <InputList
+        isOpen={isOpenList === listName}
+        ItemNumber={listData.length}
+      >
+        {listData.map((ListItem) => (
+          <InputItem
+            key={ListItem.list_id}
+            onClick={() => selectItem(ListItem.name)}
+            selected={ListItem.name === selectedItemName}
+          >
+            {ListItem.name}
+          </InputItem>
+        ))}
+      </InputList>
+    </InputBox>
+  );
+}
+
+export default ListInput;
--- src/views/Setting/index.js
+++ src/views/Setting/index.js
@@ -1,90 +1,87 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { SettingContainer, InputBox, InputList, InputItem, SelectedText, ArrowIcon } from './style';
+import React from 'react';
+import { SettingContainer } from './style';
 import InputWrap from '../../components/Input';
-import Button from '../../components/Buttons';
-import InputArrowIcon from '../../assets/input-arrow-icon.svg';
 
 const algorithmList = [
   {
-    algo_id: 124,
-    algo_name: '수학',
+    id: 124,
+    name: '수학',
   },
   {
-    algo_id: 102,
-    algo_name: '구현',
+    id: 102,
+    name: '구현',
   },
   {
-    algo_id: 25,
-    algo_name: '다이나믹 프로그래밍(DP)',
+    id: 25,
+    name: '다이나믹 프로그래밍(DP)',
   },
   {
-    algo_id: 175,
-    algo_name: '자료 구조',
+    id: 175,
+    name: '자료 구조',
   },
   {
-    algo_id: 7,
-    algo_name: '그래프 이론',
+    id: 7,
+    name: '그래프 이론',
   },
   {
-    algo_id: 33,
-    algo_name: '그리디 알고리즘',
+    id: 33,
+    name: '그리디 알고리즘',
   },
   {
-    algo_id: 158,
-    algo_name: '문자열',
+    id: 158,
+    name: '문자열',
   },
   {
-    algo_id: 125,
-    algo_name: '브루트포스 알고리즘',
+    id: 125,
+    name: '브루트포스 알고리즘',
   },
   {
-    algo_id: 11,
-    algo_name: '그래프 탐색',
+    id: 11,
+    name: '그래프 탐색',
   },
   {
-    algo_id: 97,
-    algo_name: '정렬',
+    id: 97,
+    name: '정렬',
   },
   {
-    algo_id: 100,
-    algo_name: '기하학',
+    id: 100,
+    name: '기하학',
   },
   {
-    algo_id: 95,
-    algo_name: '정수론',
+    id: 95,
+    name: '정수론',
   },
   {
-    algo_id: 120,
-    algo_name: '트리',
+    id: 120,
+    name: '트리',
   },
   {
-    algo_id: 109,
-    algo_name: '애드 혹',
+    id: 109,
+    name: '애드 혹',
   },
   {
-    algo_id: 65,
-    algo_name: '세그먼트 트리',
+    id: 65,
+    name: '세그먼트 트리',
   },
   {
-    algo_id: 12,
-    algo_name: '이분 탐색',
+    id: 12,
+    name: '이분 탐색',
   },
   {
-    algo_id: 121,
-    algo_name: '사칙연산',
+    id: 121,
+    name: '사칙연산',
   },
   {
-    algo_id: 141,
-    algo_name: '시뮬레이션',
+    id: 141,
+    name: '시뮬레이션',
   },
   {
-    algo_id: 126,
-    algo_name: '너비 우선 탐색',
+    id: 126,
+    name: '너비 우선 탐색',
   },
   {
-    algo_id: 127,
-    algo_name: '깊이 우선 탐색',
+    id: 127,
+    name: '깊이 우선 탐색',
   },
 ];
 
@@ -108,88 +105,15 @@ const problemTierList = [
 ];
 
 const Setting = () => {
-  const [originalAlgorithmId, setOriginalAlgorithmId] = useState('');
-  const [algorithmId, setAlgorithmId] = useState('');
-  const [algorithmName, setAlgorithmName] = useState('');
-
-  const [isDisabled, setIsDisabled] = useState(true);
-  const [isOpenList, setOpenList] = useState('');
-
-  useEffect(() => {
-    const url = 'https://www.iflab.run/api/algorithm';
-    axios.get(url)
-      .then((response) => {
-        setOriginalAlgorithmId(response.data);
-        setAlgorithmName(algorithmList.find((item) => item.algo_id === response.data).algo_name);
-      })
-      .catch((error) => {
-        console.log(error);
-      }
-    );
-  }, []);
-
-  const openList = (id) => {
-    if(id === isOpenList) {
-      setOpenList('');
-      return;
-    }
-    setOpenList(id);
-  }
-
-  const selectAlgorithm = (algorithmName) => {
-    setAlgorithmId(algorithmList.find((item) => item.algo_name === algorithmName).algo_id);
-    setAlgorithmName(algorithmName);
-    setOpenList('');
-    setIsDisabled(false);
-  }
-
-  const clickInitButton = () => {
-    setIsDisabled(true);
-    setAlgorithmId(originalAlgorithmId);
-    setAlgorithmName(algorithmList.find((item) => item.algo_id === originalAlgorithmId).algo_name);
-  }
-
-  const clickSaveButton = () => {
-    const url = 'https://www.iflab.run/api/algorithm';
-    axios.patch(url, {
-      id: algorithmId,
-      name: algorithmName,
-    })
-    setIsDisabled(true);
-    setOriginalAlgorithmId(algorithmId);
-    setAlgorithmId('');
-  }
 
   return (
     <SettingContainer>
-      <InputWrap label={'알고리즘 변경'}>
-        <InputBox onClick={() => openList('algorithm')}>
-          <SelectedText>{algorithmName}</SelectedText>
-          <ArrowIcon src={InputArrowIcon} selected={isOpenList === 'algorithm'} />
-          <InputList
-            isOpen={isOpenList === 'algorithm'}
-            ItemNumber={algorithmList.length}
-          >
-            {algorithmList.map((ListItem) => (
-              <InputItem
-                key={ListItem.list_id}
-                onClick={() => selectAlgorithm(ListItem.algo_name)}
-                selected={ListItem.algo_name === algorithmName}
-              >
-                {ListItem.algo_name}
-              </InputItem>
-            ))}
-          </InputList>
-        </InputBox>
-        <Button
-          color={"blue"}
-          type={isDisabled ? "disabled" : "active"}
-          size={"small"}
-          onClick={clickSaveButton}
-        >
-          저장
-        </Button>
-      </InputWrap>
+      <InputWrap
+        label={'알고리즘 변경'}
+        apiURL={'algorithm'}
+        listName={'algorithm'}
+        listData={algorithmList}
+      />
     </SettingContainer>
   );
 };
