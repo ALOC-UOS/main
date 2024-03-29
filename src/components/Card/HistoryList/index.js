d78e4a407bb18a1ccdcf010fc2c46e7d3337940f
Dodolist <dodolist3@gmail.com>
2024-03-30 00:37:09 +0900

    feat: History API 연동

--- src/components/Card/HistoryList/index.js
+++ src/components/Card/HistoryList/index.js
@@ -1,187 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { HistoryListContainer, CardWrapper, CardSubscription, CardContent, Icon, Text } from './style';
 import AllSolveIcon from '../../../assets/all-solve-icon.svg';
-import MemberDotIcon from '../../../assets/member-dot-icon.svg';
-import MemberPlusIcon from '../../../assets/member-plus-icon.svg';
+import changeRank from '../../../assets/change-rank-icon.svg';
+import plusMember from '../../../assets/plus-member-icon.svg';
 
 const HistoryList= () => {
-  const HistoryData = [
-    {
-      subscription: '2024년 3월 26일 화요일',
-      contents: [
-        {
-          Icon: MemberPlusIcon,
-          Name: '나윤서'
-        },
-      ]
-    },
-    {
-      subscription: '2024년 3월 25일 월요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '이강민',
-          Tier: '골드 IV'
-        }
-      ]
-    },
-    {
-      subscription: '2024년 3월 23일 토요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '박주영',
-          Tier: '브론즈 III'
-        }
-      ]
-    },
-    {
-      subscription: '2024년 3월 20일 수요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '박주영',
-          Tier: '브론즈 IV'
-        }
-      ]
-    },
-    {
-      subscription: '2024년 3월 19일 화요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '박나은',
-          Tier: '골드 V'
-        }
-      ]
-    },
-    {
-      subscription: '2024년 3월 18일 월요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '문재영',
-          Tier: '골드 IV'
-        }
-      ]
-    },
-    {
-      subscription: '2024년 3월 14일 목요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '김영진',
-          Tier: '골드 V'
-        },
-      ]
-    },
-    {
-      subscription: '2024년 3월 11일 월요일',
-      contents: [
-        {
-          Icon: MemberDotIcon,
-          Name: '조수아',
-          Tier: '골드 IV'
-        },
-        {
-          Icon: MemberDotIcon,
-          Name: '박상윤',
-          Tier: '실버 I'
-        }
-      ]
-    },
-    {
-      subscription: '2024년 3월 8일 금요일',
-      contents: [
-        {
-          Icon: MemberPlusIcon,
-          Name: '박주영'
-        },
-      ]
-    },
-    {
-      subscription: '2024년 3월 7일 목요일',
-      contents: [
-        {
-          Icon: MemberPlusIcon,
-          Name: '정다현'
-        },
-      ]
-    },
-    /*
-    {
-      subscription: '2024년 3월 4일 월요일',
-      contents: [
-        {
-          Icon: MemberPlusIcon,
-          Name: '조종빈, 김하람'
-        },
-        {
-          Icon: MemberDotIcon,
-          Name: '이종우',
-          Tier: '골드 III'
-        },
-        {
-          Icon: MemberDotIcon,
-          Name: '박나은',
-          Tier: '실버 I'
-        }
-      ]
-    },
+  const [historyData, setHistoryData] = useState([]);
 
-    {
-      subscription: '2024년 3월 3일 월요일',
-      contents: [{
-        Icon: MemberPlusIcon,
-        Name: '김영진'
-      }]
-    },
-    {
-      subscription: '2024년 3월 2일 토요일',
-      contents: [{
-        Icon: MemberDotIcon,
-        Name: '이강민',
-        Tier: '골드 V'
-      }]
-    },
-    */
-  ];
+  useEffect(() => {
+    loadHistoryData();
+  }, []);
+  
+  const loadHistoryData = () => {
+    let url = 'https://www.iflab.run/api/show/histories';
+    axios.get(url)
+      .then(response => {
+        setHistoryData(response.data);
+      })
+      .catch(error => {
+        console.error('API 요청 중 오류 발생:');
+      });
+  }
 
+  const getRank = (rank) => {
+    const tier = parseInt(rank / 10);
+    const level = rank % 10;
+    const rankText = ['I', 'II', 'III', 'IV', 'V'];
+  
+    switch (tier) {
+      case 1:
+        return `브론즈 ${rankText[level - 1]}`;
+      case 2:
+        return `실버 ${rankText[level - 1]}`;
+      case 3:
+        return `골드 ${rankText[level - 1]}`;
+      case 4:
+        return `플래티넘 ${rankText[level - 1]}`;
+      default:
+        return 'Unranked';
+    }
+  }
 
-  const getTier = (tier) => {
-    if (tier.includes('브론즈')) {
-      return 'bronze';
-    } else if (tier.includes('실버')) {
-      return 'silver';
-    } else if (tier.includes('골드')) {
-      return 'gold';
-    } else if (tier.includes('플래티넘')) {
-      return 'platinum';
+  const getTier = (rank) => {
+    const tier = parseInt(rank / 10);
+  
+    switch (tier) {
+      case 1:
+        return 'bronze';
+      case 2:
+        return 'silver';
+      case 3:
+        return 'gold';
+      case 4:
+        return 'platinum';
+      default:
+        return 'Unranked';
     }
   }
   return (
     <HistoryListContainer>
-      {HistoryData.map((data, index) => {
+      {historyData.map((data, index) => {
         return (
           <CardWrapper key={index}>
             <CardSubscription>
-              {data.subscription}
+              {data.date}
             </CardSubscription>
             {data.contents.map((content, index) => {
               return (
                 <CardContent key={index}>
-                  <Icon src={content.Icon} />
-                  {content.Icon === MemberPlusIcon ? (
+                  <Icon src={content.icon === 'plusMember' ? plusMember : content.icon === 'changeRank' ? changeRank : AllSolveIcon} />
+                  {content.icon === 'plusMember' ? (
                     <Text>
-                      <Text textColor={'blue'}>{content.Name}</Text>님이 &nbsp;
+                      <Text textColor={'blue'}>{content.name}</Text>님이 &nbsp;
                       <Text textColor={'blue'}>모임</Text>에 합류했어요!
                     </Text>
                   ) :
-                    content.Icon === MemberDotIcon ? (
+                    content.icon === 'changeRank' ? (
                       <Text>
-                        <Text textColor={'blue'}>{content.Name}</Text>님이 &nbsp;
-                        <Text textColor={content.Tier && getTier(content.Tier)}>
-                          {content.Tier}
+                        <Text textColor={'blue'}>{content.name}</Text>님이 &nbsp;
+                        <Text textColor={content.rank && getTier(content.rank)}>
+                          {getRank(content.rank)}
                         </Text>을 달성했어요!
                       </Text>
                   ) : (
