78600ca017fc52eaab1aecaf55367c12d3e7f3b0
Dodolist <dodolist3@gmail.com>
2024-05-07 15:26:18 +0900

    fix: 색상 추가 시 여러개 설정가능

--- src/views/Setting/index.js
+++ src/views/Setting/index.js
@@ -130,8 +130,28 @@ const colorAddList = [
     placeholder: 'Name'
   },
   {
-    id: 'color',
-    placeholder: 'Color'
+    id: 'color1',
+    placeholder: 'Color1'
+  },
+  {
+    id: 'color2',
+    placeholder: 'Color2'
+  },
+  {
+    id: 'color3',
+    placeholder: 'Color3'
+  },
+  {
+    id: 'color4',
+    placeholder: 'Color4'
+  },
+  {
+    id: 'color5',
+    placeholder: 'Color5'
+  },
+  {
+    id: 'degree',
+    placeholder: 'Degree'
   },
   {
     id: 'category',
